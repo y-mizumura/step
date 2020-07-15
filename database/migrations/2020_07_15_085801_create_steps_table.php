c06f334ff15a314b4a4358f2d180ccd3a039f1b3
Yuta Mizumura <yutam7@gmail.com>
2020-07-15 18:15:18 +0900

    マイグレーションファイルの作成／モデルクラス作成

---
+++ database/migrations/2020_07_15_085801_create_steps_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStepsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('steps', function (Blueprint $table) {
+            $table->id();
+            $table->integer('mission_id')->unsigned();
+            $table->date('date');
+            $table->integer('level')->unsigned();
+            $table->string('memo', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('steps');
+    }
+}
