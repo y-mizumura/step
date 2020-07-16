b955dd358781d6632d9d540f751e10c45e7da20a
Yuta Mizumura <yutam7@gmail.com>
2020-07-17 03:26:00 +0900

    missions→users 外部キー制約 追加

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function missions()
+    {
+        return $this->hasMany('App\Mission');
+    }
 }
