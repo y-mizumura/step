2a8a0f3a742a38eb62045d13f1e20c2d83123a16
Yuta Mizumura <yutam7@gmail.com>
2020-07-20 01:43:30 +0900

    スコア 小数点入力に修正（サーバにcomposerがうまく入らなかったので。）

--- database/migrations/2020_07_15_085801_create_steps_table.php
+++ database/migrations/2020_07_15_085801_create_steps_table.php
@@ -17,7 +17,7 @@ class CreateStepsTable extends Migration
             $table->id();
             $table->bigInteger('mission_id')->unsigned();
             $table->date('date');
-            $table->integer('score')->unsigned();
+            $table->decimal('score', 8, 1);
             $table->string('memo', 20)->nullable();
             $table->timestamps();
         });
